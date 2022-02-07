310e4af9146178381d88a7b82893b45dbcdd751f
vijayjain01 <98397527+vijayjain01@users.noreply.github.com>
2022-02-07 16:35:22 +0530

    Add files via upload

---
+++ assignment 2.js
@@ -0,0 +1,17 @@
+let num = "12562";
+if (num % 2 == 0){
+    console.log(" Given number is even");
+}else{
+    console.log("Given number is odd");
+}
+
+// ===============================================
+
+let n = 64;
+if (n > 0 && Math.sqrt(n) % 1 === 0 ) {
+    console.log("perfect");
+}else
+{
+    console.log("Not perfect");
+}
+
